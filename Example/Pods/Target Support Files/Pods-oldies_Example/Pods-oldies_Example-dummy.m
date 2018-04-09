76d03f73d5225a9a460f3a6bfff61ca06397d819
Antoine Cœur <antoine.coeur@ef.com>
2018-04-09 20:04:27 +0800

    Adding :testspecs for iOS 8.x

---
+++ Example/Pods/Target Support Files/Pods-oldies_Example/Pods-oldies_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_oldies_Example : NSObject
+@end
+@implementation PodsDummy_Pods_oldies_Example
+@end
---
+++ Example/Pods/Target Support Files/oldies/oldies-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_oldies : NSObject
+@end
+@implementation PodsDummy_oldies
+@end
